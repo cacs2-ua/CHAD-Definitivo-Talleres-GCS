c9a025b9ff881b9d212e7f350827280deb64ab85
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-05-20 17:26:27 +0200

    headers done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
-import { Component } from '@angular/core';
-import { Platform, ToastController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { MenuController, ToastController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-root',
+  selector   : 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  standalone: false,
+  styleUrls  : ['app.component.scss'],
+  standalone : false
 })
-export class AppComponent {
-  constructor(private platform: Platform, private toastController: ToastController) {
-    this.initializeApp();
+export class AppComponent implements OnInit {
+
+  menuOptions: any[] = [];
+
+  constructor(
+    private menuCtrl : MenuController,
+    private toastCtrl: ToastController
+  ) {}
+
+  async ngOnInit() {
+    await this.loadMenu();
+    this.presentToast('¡Bienvenido a la Wiki Star-Wars!');
   }
 
-  async initializeApp() {
-    await this.platform.ready();
-    const toast = await this.toastController.create({
-      message: 'Welcome to the Star Wars Wiki App!',
-      duration: 2000,
-      position: 'top',
-      color: 'primary'
-    });
-    toast.present();
+  private async loadMenu() {
+    const res        = await fetch('assets/data/menu.json');
+    this.menuOptions = await res.json();
+  }
+
+  /** Cierra el menú al pulsar una opción */
+  closeMenu() { this.menuCtrl.close('principal'); }
+
+  /** Toast genérico */
+  private async presentToast(msg: string) {
+    const t = await this.toastCtrl.create({ message: msg, duration: 2000, position: 'bottom' });
+    t.present();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule }                         from '@angular/core';
+import { BrowserModule }                    from '@angular/platform-browser';
+import { RouteReuseStrategy }               from '@angular/router';
+import { IonicModule, IonicRouteStrategy }  from '@ionic/angular';
+import { IonicStorageModule }               from '@ionic/storage-angular';    // ← Aquí
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, NgbModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),    // ← Asegúrate de tener esta línea
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
