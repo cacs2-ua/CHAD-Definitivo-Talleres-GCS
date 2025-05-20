5e2c4dfff1d6f6518dcffec69d9f569516005020
Cristian Andrés Córdoba Silvestre <cacs2@alu.ua.es>
2025-05-20 16:49:11 +0200

    .

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.starwarstios.app',
+  appName: 'appIonic',
+  webDir: 'www',
+};
+
+export default config;
